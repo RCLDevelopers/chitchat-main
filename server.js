71fc7f0af0c88700ed8cda0f83b44f0c8c4423f8
RCLDevelopers <rasshtech@gmail.com>
2024-05-22 08:36:42 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const socketio = require('socket.io')
+const PORT = process.env.PORT || 3000;
+const expressServer = app.listen(PORT);
+
+const io = socketio(expressServer);
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+//code of socket to make connection 
+
+io.on('connection', (socket) => {
+    console.log('Connected')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+
+})
